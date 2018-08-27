50e767ca5089a151bbff9c4e2e16d0cbe0d1974e
S L <lordstepheng@gmail.com>
2018-08-27 00:30:37 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,8 +26,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["am",4,6,8]
-  array[0]
+  array = [,4,6,"am", 8]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
