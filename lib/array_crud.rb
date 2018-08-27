bd845c0ab7c85a3bce503a3abe204b8c6b5cce14
S L <lordstepheng@gmail.com>
2018-08-27 00:26:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array["wow", "I", "am", "really", "learning", "arrays!"]
+  array["wow", "I", "am"]
   array[2]
 end
 
