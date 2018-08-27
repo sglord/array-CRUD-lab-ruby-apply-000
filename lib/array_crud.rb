eb736b6524c90358602656dbe7734e4ac333af62
S L <lordstepheng@gmail.com>
2018-08-27 00:10:04 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [2,4,6,8]
-  array.unshift(0)
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = [2,4,6,8]
+  array.unshift(0)
 end
 
 def remove_element_from_end_of_array(array)
